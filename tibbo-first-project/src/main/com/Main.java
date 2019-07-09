ee98ae93313931b86ea33a5d5da1dd2681ca9d56
Ilya Belov <Mireech@yandex.ru>
2019-07-09 15:51:22 +0300

    is a final solution?

--- tibbo-first-project/src/main/com/Main.java
+++ tibbo-first-project/src/main/com/Main.java
@@ -5,42 +5,36 @@ public class Main
   {
     System.out.println("Hello world!");
   }
+    public static final String str = "This is the name of Main class";
   public Integer plus(int[] array)
   {
-      int summ=0;
-      for(int i=0;i<array.length;i++)
-      {
-          summ+=array[i];
-      }
+      int summ = 0;
+      for(int i:array)
+          summ += i;
       return summ;
   }
   
   public String encode(byte[] byteArray)
   {
-      String solution = new String(byteArray);
-      return solution;
+      return new String(byteArray);
   }
   
   public Boolean stringContains(String value, String subString)
   {
-      int index = value.indexOf(subString);
-      if(index>=0)
+      if(value.indexOf(subString)>=0)
           return true;
       else
           return false;
-
   }
   
   @Override
   public String toString()
   {
-      String solution = "This is the name of Main class";
-      return solution;
+      return str;
   }
   
   public static Main getInstance()
   {
-      Main main = new Main();
-      return main;
+      return new Main();
   }
 }

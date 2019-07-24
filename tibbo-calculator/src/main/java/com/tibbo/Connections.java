55e1d03e45a88a6d20151996f70ef77deef2bc91
LourenceRin <duffel733@yandex.ru>
2019-07-24 13:34:48 +0300

    Calculator

--- tibbo-calculator/src/main/java/com/tibbo/Connections.java
+++ tibbo-calculator/src/main/java/com/tibbo/Connections.java
@@ -4,7 +4,7 @@ import com.tibbo.Server;
 import java.io.*;
 import java.net.Socket;
 
-public class Connections extends Thread {//почему connections от калькулятора находится в пакете баз данных? либо новый пакет сделай, либо просто вынеси туда где и свервер
+public class Connections extends Thread {
     private Socket socket;
     private Server INSTANCE;
     public Connections(Socket socket, Server INSTANCE)
